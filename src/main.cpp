4958ff4cdf4accdc4761352bc27c864de52686de
shuumo <arnav.divesh.singh@gmail.com>
2024-12-19 11:32:04 +1100

    fixed tracking issues with certain files, created basic QT6 login screen in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 #include <QtWidgets>
 
+/*
+ * Login GUI Stuff using QT6
+ *
+*/
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
     QWidget window;
     window.resize(500, 500);
-    window.show();
+    window.setMinimumSize(500,500);
     window.setWindowTitle(QApplication::translate("password_manager", "Password Manager"));
+
+    QLabel *label = new QLabel(QApplication::translate("master_entry", "Master Key: "));
+    QLineEdit *lineEdit = new QLineEdit();
+    lineEdit->setEchoMode(QLineEdit::Password);
+    
+    QPushButton *button = new QPushButton(QApplication::translate("childwidget", "log in"), &window); 
+    button->setFixedSize(100, 50);
+
+    QHBoxLayout *keyEntryLayout = new QHBoxLayout();
+    keyEntryLayout->addSpacing(100);
+    keyEntryLayout->addWidget(label);
+    keyEntryLayout->addWidget(lineEdit);
+    keyEntryLayout->addSpacing(100);
+    
+    QHBoxLayout *buttonLayout = new QHBoxLayout();
+    buttonLayout->addWidget(button);
+
+    QVBoxLayout *verticalLayout = new QVBoxLayout();
+    verticalLayout->addLayout(keyEntryLayout);
+    verticalLayout->addLayout(buttonLayout);
+    verticalLayout->setSpacing(0);
+
+    window.setLayout(verticalLayout);    
+
+    window.show();
     return app.exec();
 }
 
